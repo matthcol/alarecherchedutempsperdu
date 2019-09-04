17a2ca97871315e7d1357e8a1643d841b32b91f4
Matthias Colin <matthias.colin@gmail.com>
2019-09-04 17:02:11 +0200

    locale

--- src/test/java/time/test/TestDatetime.java
+++ src/test/java/time/test/TestDatetime.java
@@ -5,6 +5,9 @@ import static org.junit.jupiter.api.Assertions.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
 
 import org.junit.jupiter.api.Test;
 
@@ -20,6 +23,19 @@ class TestDatetime {
 	void testDate() {
 		var queljouronest = LocalDate.now();
 		System.out.println(queljouronest);
+		System.out.println(
+				queljouronest.format(DateTimeFormatter.ofPattern("eeee dd/MM/Y")));
+	}
+	
+	@Test
+	void testDateInternational() {
+		ZoneId argTz = ZoneId.of("America/Buenos_Aires");
+		Locale argLocale = new Locale("arg", "esp");
+		System.out.println(argLocale);
+		var quand = LocalDateTime.now(argTz);
+		System.out.println(quand);
+		System.out.println(
+				quand.format(DateTimeFormatter.ofPattern("eeee dd/MM/Y", argLocale)));
 	}
 	
 	@Test
@@ -27,5 +43,8 @@ class TestDatetime {
 		var onestquand = LocalDateTime.now();
 		System.out.println(onestquand);
 	}
+	
+	
+	
 
 }
