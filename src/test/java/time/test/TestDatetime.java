e9c96aa6441bb2f85527ef0422c8b4619651f55a
Matthias Colin <matthias.colin@gmail.com>
2019-09-04 16:03:12 +0200

    date du jour

--- src/test/java/time/test/TestDatetime.java
+++ src/test/java/time/test/TestDatetime.java
@@ -2,6 +2,7 @@ package time.test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 import org.junit.jupiter.api.Test;
@@ -12,6 +13,12 @@ class TestDatetime {
 	void testTime() {
 		var quelleheureestil = LocalTime.now();
 		System.out.println(quelleheureestil);
+	}
+	
+	@Test
+	void testDate() {
+		var queljouronest = LocalDate.now();
+		System.out.println(queljouronest);
 	}
 
 }
