8551ef28e91951c1519159b107b4e812ea67fc47
Matthias Colin <matthias.colin@gmail.com>
2019-09-05 11:13:53 +0200

    add parsing date

--- src/test/java/time/test/TestDatetime.java
+++ src/test/java/time/test/TestDatetime.java
@@ -9,12 +9,16 @@ import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.List;
 import java.util.Locale;
 
 import org.junit.jupiter.api.Test;
 
 class TestDatetime {
 
+	public static final DateTimeFormatter FORMAT_DATE_FR =
+			DateTimeFormatter.ofPattern("dd/MM/yyyy");
+	
 	@Test
 	void testTime() {
 		var quelleheureestil = LocalTime.now();
@@ -26,7 +30,14 @@ class TestDatetime {
 		var queljouronest = LocalDate.now();
 		System.out.println(queljouronest);
 		System.out.println(
-				queljouronest.format(DateTimeFormatter.ofPattern("eeee dd/MM/Y")));
+				queljouronest.format(
+						DateTimeFormatter.ofPattern("dd/MM/yyyy")));
+		System.out.println(
+				queljouronest.format(
+						DateTimeFormatter.ofPattern("d/M/yyyy")));
+		System.out.println(
+				queljouronest.format(
+						DateTimeFormatter.ofPattern("d/M/yy")));
 	}
 	
 	@Test
@@ -50,7 +61,31 @@ class TestDatetime {
 		System.out.println(onestquand);
 	}
 	
-	
-	
+	@Test
+	void testParsing() {
+		String dateIso = "2019-09-07";
+		String dateFr = "07/09/2019";
+		String dateUk= "09/07/2019";
+		String dateUkShort = "9/7/19";
+		
+		LocalDate date1 = LocalDate.parse(dateIso);
+		LocalDate date2 = LocalDate.parse(dateFr, FORMAT_DATE_FR);
+		LocalDate date3 = LocalDate.parse(dateUk, 
+				DateTimeFormatter.ofPattern("MM/dd/yyyy"));
+		LocalDate date4 = LocalDate.parse(dateUkShort, 
+				DateTimeFormatter.ofPattern("M/d/yy"));
+		List<LocalDate> dates = List.of(date1, date2, date3, date4);
+		for (var date : dates) {
+			System.out.println("année : " + date.getYear());
+			System.out.println("mois : " + date.getMonth());
+			System.out.println("jour : " + date.getDayOfMonth());
+		}
+	}
 
+	@Test
+	void testDateFr() {
+		String dateFr = "07/09/2019";
+		LocalDate date = LocalDate.parse(dateFr, FORMAT_DATE_FR);
+		
+	}
 }
